728b14fa53c33eef4148cca73415c39406174927
maurosergio14 <50503862+maurosergio14@users.noreply.github.com>
2020-05-24 16:30:53 -0300

    trabajo practico uml_bd

--- src/CONTROLADOR/Alumno.java
+++ src/CONTROLADOR/Alumno.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CONTROLADOR;
+
+import java.time.LocalDate;
 
 /**
  *
  * @author Sergio
  */
 public class Alumno {
+    int id=-1;
+   private String nombre;
+   private String apellido;
+   private String mail;
+   private LocalDate fecNac;
+   private boolean cursando;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellido, String mail, LocalDate fecNac, boolean cursando) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.fecNac = fecNac;
+        this.cursando = cursando;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public LocalDate getFecNac() {
+        return fecNac;
+    }
+
+    public void setFecNac(LocalDate fecNac) {
+        this.fecNac = fecNac;
+    }
+
+    public boolean isCursando() {
+        return cursando;
+    }
+
+    public void setCursando(boolean cursando) {
+        this.cursando = cursando;
+    }
+    
+    @Override
+    public String toString() {
+        return "Alumno{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", cursando=" + cursando + '}';
+    }
+   
+    
     
 }
--- src/MODELO/Alumno_Bdata.java
+++ src/MODELO/Alumno_Bdata.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MODELO;
+
+import java.sql.Connection;
 
 /**
  *
  * @author Sergio
  */
 public class Alumno_Bdata {
+    private Connection conectar;
+    
+    public Alumno_Bdata() {
+       
+       }
+    
+    
+    
     
 }
